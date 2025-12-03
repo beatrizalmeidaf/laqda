390c8f374098b30a1e5c234a0f2e89f9535c055e
beatrizalmeidaf <beatrizalmeida.ufg@gmail.com>
2025-12-03 13:39:26 -0300

    new branch

--- run_qevasion.sh
+++ run_qevasion.sh
@@ -10,17 +10,13 @@ BASE_SAVE_DIR="${RAID_BASE_PATH}/QEvasion_Augmented_Results"
 echo "Rodando LAQDA (com BGE-M3) na Tarefa de Evasão (5-Fold)"
 
 MODEL_NAME="BAAI/bge-m3"
-K_SHOT=55 # O k-shot que você quer testar
+K_SHOT=55 
 
-# --- Loop pelos 5 Folds ---
 for fold in 01 02 03 04 05; do
-    echo "--- Verificando/Rodando Fold $fold (k-shot = $K_SHOT) ---"
+    echo "Verificando/Rodando Fold $fold (k-shot = $K_SHOT)"
     
-    # --- CORREÇÃO IMPORTANTE ---
-    # Define o DATA_DIR para o caminho completo do fold atual
     DATA_DIR="${BASE_DATA_DIR}/${fold}" # Ex: "data/QEvasionCorpus/few_shot/01"
     
-    # Salva cada fold em uma subpasta de resultados
     SAVE_DIR="${BASE_SAVE_DIR}/kshot_${K_SHOT}_fold_${fold}" 
     RESULT_FILE="${SAVE_DIR}/result.csv"
     
@@ -32,7 +28,6 @@ for fold in 01 02 03 04 05; do
         
         commont="LAQDA-BGE-M3-Fold${fold}-k${K_SHOT}"
 
-        # O main.py agora recebe o caminho completo, ex: data/QEvasionCorpus/few_shot/01
         python src/main.py \
             --dataset "QEvasionTask2" \
             --dataFile "$DATA_DIR" \
